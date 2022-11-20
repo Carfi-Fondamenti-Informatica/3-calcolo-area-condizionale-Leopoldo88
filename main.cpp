88be9b6e97c8b1d14676b5791a860d2752a8dfa0
Leopoldo88 <114876690+Leopoldo88@users.noreply.github.com>
2022-11-20 11:27:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,27 @@
 using namespace std;
 
 int main() {
+float a=0;
+float b=0;
+int opzione=0;
+cin>>a>>b>>opzione;
+if(opzione<0||opzione>2){
+cout<<"opzione non valida"<<endL;
+return 0;
+}
+switch(opzione){
+   case 0:
+cout<<(a*b)/2<<endL;
+break;
+   case 1:
+ cout<<a*a<<endL;
+ break;
+   case 2:
+  cout<<a*b<<endL;
+ break;
+ }
+ 
+
+
    return 0;
 }
